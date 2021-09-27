19d242e558bbf05f0e4ec97ea3843203edd4e6b4
jmarquez-urq <jmarquez.urquiza@gmail.com>
2021-09-27 00:30:07 -0300

    Login de usuario

---
+++ .envPUBLICO.php
@@ -0,0 +1,9 @@
+<?php
+function credenciales() {
+    return [
+        'usuario'=>'juan' ,
+        'clave'=>'supersecreto',
+        'servidor'=>'localhost',
+        'base_de_datos'=>'banco'
+    ];
+}
---
+++ clases/ControladorSesion.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'Usuario.php';
+require_once 'RepositorioUsuario.php';
+
+class ControladorSesion
+{
+    protected $usuario = null;
+
+    public function login($nombre_usuario, $clave)
+    {
+        $repo = new RepositorioUsuario();
+        $usuario = $repo->login($nombre_usuario, $clave);
+        //Si falló el login:
+        if ($usuario === false) {
+            return [false, "Error de credenciales"];
+        } else {
+            session_start();
+            $_SESSION['usuario'] = serialize($usuario);
+            return [true, "Usuario autenticado correctamente"];
+        }
+    }
+}
